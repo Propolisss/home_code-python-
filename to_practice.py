fc4da9bba1912f2ff1dd4e93499cf943cddfa4e7
Propolisss <artem89296145449@gmail.com>
2022-10-01 01:14:32 +0300

    30.09.2022

--- to_practice.py
+++ to_practice.py
@@ -1,9 +1,9 @@
-ch = input().split()
-ss = list(int(i) for i in ch)
+with open('17-4.txt') as file:
+    nums = [int(i) for i in file]
 
-for i in range(len(ss)):
-    for j in range(len(ss)):
-        if i != j and ss[i] == ss[j]:
-            break
-    else:
-        print(ss[i])
+ans = []
+
+for i in range(len(nums)):
+    if ((nums[i] % 10 == 2) or (nums[i] % 10 == 7)) and (nums[i] % 3 == 0 and nums[i] % 11 == 0):
+        ans.append(nums[i])
+print(len(ans), min(ans))
