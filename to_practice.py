1ccc565b88e5b725db7ee0f8237f250b72d0dbf0
Propolisss <artem89296145449@gmail.com>
2022-10-14 18:38:31 +0300

    v2

--- to_practice.py
+++ to_practice.py
@@ -1,21 +1,31 @@
+from functools import lru_cache
+
 delt = []
 nums = []
 
+#@lru_cache
 def f(start, end, current):
     global nums
     global delt
+    if len(delt) > 0:
+        nums = delt[-1][:]
     nums.append(current)
     delt.append(nums[:])
     for i in range(1, len(delt[-1])):
         if delt[-1][i - 1] == 1 and delt[-1][i] == 1:
             del delt[-1]
+            #delt.clear()
             nums.clear()
             return 0
-    if start > end or start == 30 or start == 40:
+    if start > end or start == 33:
+        #delt.clear()
+        nums.clear()
         return 0
     if start == end:
+        #delt.clear()
+        nums.clear()
         return 1
     else:
         return f(start * 2, end, 1) + f(start + 1, end, 2) + f(start + 3, end, 3)
 
-print(f(3, 20, 0) * f(20, 60, 0))
+print(f(2, 18, 0) * f(18, 51, 0))
