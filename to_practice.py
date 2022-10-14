43a3fbe618afe2fc14a6711ce10a9dca1a893458
Propolisss <artem89296145449@gmail.com>
2022-10-14 16:33:04 +0300

    v2

--- to_practice.py
+++ to_practice.py
@@ -1,22 +1,21 @@
 delt = []
 nums = []
 
-
 def f(start, end, count, current):
     global nums
     global delt
     nums.append(current)
     delt.append(nums[:])
     for i in range(1, len(delt[-1])):
-        if delt[-1][i - 1] == 2 and delt[-1][i] == 2:
+        if delt[-1][i - 1] == 1 and delt[-1][i] == 1:
             del delt[-1]
             nums.clear()
             return 0
-    if start > end:
+    if start > end or start == 30 or start == 40:
         return 0
     if start == end:
         return 1
     else:
-        return f(start * 3, end, count + 1, 2) + f(start + 1, end, count + 1, 1) + f(start + 2, end, count + 1, 3)
+        return f(start * 2, end, count + 1, 1) + f(start + 1, end, count + 1, 2) + f(start + 3, end, count + 1, 3)
 
-print(f(3, 30, 0, 0))
+print(f(3, 20, 0, 0) * f(20, 60, 0, 0))
