199eb69d4c05998d27153729ec81334dc3ec08be
Propolisss <artem89296145449@gmail.com>
2022-11-16 22:54:20 +0300

    24 matrix

--- to_practice.py
+++ to_practice.py
@@ -1,17 +1,35 @@
+s = [i for i in open('24-164.txt')]
+lens = []
+dic = [[0] * len(s) for _ in range(256)]
 
+maxx_len = float('-inf')
+for i in range(len(s)):
+    st = ''
+    local_max = float('-inf')
+    for j in range(len(s[i])):
+        dic[ord(s[i][j])][i] += 1
+        st = s[i][j]
+        k = j + 1
 
-def f(n):
-    if n < 2:
-        return n
-    if (n >= 2) and (n % 2 == 0):
-        return f(n // 2) + 1
-    if (n >= 2) and (n & 1):
-        return f(n * 3 + 1) + 1
+        while k < len(s[i]):
+            if st[-1] == s[i][k]:
+                st += s[i][k]
+                k += 1
+            else:
+                break
 
-count = 0
-for i in range(1, 100 + 1):
-    try:
-        if f(i) > 100:
-            count += 1
+        maxx_len = max(maxx_len, len(st))
+        local_max = max(local_max, len(st))
+    lens.append(local_max)
 
-print(count)
+row = lens.index(maxx_len)
+
+maxx_let = float('-inf')
+count_maxx_let = 0
+
+for i in range(len(dic)):
+    if dic[i][row] > count_maxx_let:
+        count_maxx_let = dic[i][row]
+        maxx_let = i
+
+print(chr(maxx_let), sum(dic[maxx_let]), sep='')
