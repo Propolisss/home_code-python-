aaa1a92bf3c5366bab3771586a158b3ddc86ad4c
Propolisss <artem89296145449@gmail.com>
2023-02-23 01:20:19 +0300

    done 5245

--- practice/5245.py
+++ practice/5245.py
@@ -1,6 +1,6 @@
 file = open('26.txt')
 n = int(file.readline())
-dic = {int(sub.replace(';', ' ').split()[0]) : sub.replace(';', ' ').split()[1:] for sub in file}
+dic = {int(sub.replace(';', ' ').split()[0]) : list(map(int, sub.replace(';', ' ').split()))[1:] for sub in file}
 starter_cost = {i : int(dic[i][0]) for i in dic}
 minn = float('inf')
 maxx = float('-inf')
@@ -9,8 +9,8 @@ for i in dic:
     for j in range(1, len(dic[i])):
         r = len(dic[i]) - 1
         ind = dic[i][j]
-        dic[int(ind)][0] = int(dic[int(ind)][0]) + (int(dic[i][0]) * (1 + (int(r) / 100))) - int(dic[i][0])
-        dic[i][0] = int(dic[i][0]) * (1 - (int(r) / 100))
+        dic[ind][0] += dic[i][0] * (r / 100)
+        dic[i][0] -= dic[i][0] * (r / 100)
 
 ans = []
 for i in dic:
@@ -23,4 +23,4 @@ for i in dic:
         maxx_id = i
     ans.append([cost, i, dic[i][0], starter_cost[i]])
 ans.sort()
-print(ans)
+print(minn_id, maxx_id)
