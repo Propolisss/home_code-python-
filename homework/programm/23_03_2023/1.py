c72017e1709c4009256d966de02e2b9d6d067565
Propolisss <artem89296145449@gmail.com>
2023-03-22 23:30:15 +0300

    homework 23.03.2023

---
+++ homework/programm/23_03_2023/1.py
@@ -0,0 +1,38 @@
+file = open('26-86.txt')
+n = int(file.readline())
+dishes = dict()
+maxx = float('-inf')
+for i in file:
+    nums = [int(j) for j in i.split()]
+    maxx = max(maxx, nums[0])
+    if nums[-1] in dishes:
+        dishes[nums[-1]].append(nums[0])
+    else:
+        dishes[nums[-1]] = [nums[0]]
+
+
+maxx_time = float('-inf')
+maxx_ind = -1
+all_time = []
+for i in dishes:
+    time = []
+    dishes[i].sort()
+    for j in range(0, len(dishes[i]), 2):
+        if j == len(dishes[i]) - 1:
+            continue
+        else:
+            time.append(abs(dishes[i][j] - dishes[i][j + 1]))
+            all_time.append(sorted([dishes[i][j], dishes[i][j + 1]]))
+    if len(time) > 0:
+        if (sum(time) / len(time)) > maxx_time:
+            maxx_time = (sum(time) / len(time))
+            maxx_ind = i
+
+curr_maxx = float('-inf')
+for i in range(0, maxx - 60 + 1):
+    count = 0
+    for j in all_time:
+        if i <= j[1] < i + 60:
+            count += 1
+    curr_maxx = max(curr_maxx, count)
+print(curr_maxx, maxx_ind)
--- to_practice.py
+++ to_practice.py
@@ -1,46 +1,5 @@
-from functools import lru_cache
-file = open('practice/26.txt')
+fac = 1
 
-@lru_cache(None)
-def find_road(n):
-    if (n + 1) in dic and (n + 2) in dic:
-        return max(find_road(n + 1), find_road(n + 2))
-    elif (n + 1) in dic:
-        return find_road(n + 1)
-    elif (n + 2) in dic:
-        return find_road(n + 2)
-    else:
-        return n
-@lru_cache(None)
-def find_summ(n, summ):
-    if (n + 1) in dic and (n + 2) in dic:
-        if dic[n][0] < dic[n][1]:
-            return find_summ(n + 1, summ + dic[n][0])
-        else:
-            return find_summ(n + 2, summ + dic[n][1])
-    elif (n + 1) in dic:
-        return find_summ(n + 1, summ + dic[n][0])
-    elif (n + 2) in dic:
-        return find_summ(n + 2, summ + dic[n][0])
-    else:
-        return summ
-
-n = int(file.readline())
-nums = []
-dic = {}
-minn_id = float('inf')
-
-for i in range(n):
-    temp_arr = [int(i) for i in file.readline().split()]
-    nums.append(temp_arr)
-    dic[temp_arr[0]] = []
-    minn_id = min(minn_id, temp_arr[0])
-
-for i in range(n):
-    for j in range(1, len(nums[i])):
-        if (nums[i][0] + j) in dic:
-            dic[nums[i][0]].append(nums[i][j])
-
-maxx_id = find_road(minn_id)
-minn_summ = find_summ(minn_id, 0)
-print(minn_summ, maxx_id)
+for i in range(1, 90 + 1):
+    fac *= i
+print(oct(fac)[2:])
